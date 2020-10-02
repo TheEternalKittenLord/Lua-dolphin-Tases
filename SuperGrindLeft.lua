4d66b8db39ffb5ee30e6777dd3e8b5675f506403
TheEternalKittenLord <68739165+TheEternalKittenLord@users.noreply.github.com>
2020-10-02 22:47:10 +0100

    SuperGrindLeft.lua
    Allows you too supergrind to the left, upon executing

---
+++ SuperGrindLeft.lua
@@ -0,0 +1,17 @@
+local frame = 1
+function onScriptStart()
+    frame = 1
+end
+
+function onScriptCancel() end
+
+function onScriptUpdate()
+	  frame += 1
+    if tostring(frame/2):find(".") == nil then
+        SetMainStickX(0)
+        PressButton("B")
+    else
+       SetMainStickX(128)
+       ReleaseButton("B")
+    end
+end
